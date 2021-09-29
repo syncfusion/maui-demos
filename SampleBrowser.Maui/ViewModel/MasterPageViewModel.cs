13f5bec8978f1cb498e5db4a6ffdcf78a4d776cd
Paul Anderson <42271912+PaulAnderson02@users.noreply.github.com>
2021-09-30 02:00:53 +0530

    Added sample browser files

---
+++ SampleBrowser.Maui/ViewModel/MasterPageViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.ObjectModel;
+using System.Reflection;
+using Microsoft.Maui.Controls;
+
+namespace SampleBrowser.Maui
+{
+    public class MasterPageViewModel
+    {
+        #region fields
+
+        //Navigation Drawer links
+        private ObservableCollection<NavigationLinkModel> applinks;
+
+        #endregion
+
+        #region ctor
+
+        public MasterPageViewModel()
+        {
+            GenerateNavigationLinks();
+        }
+
+        #endregion
+
+        #region properties
+
+        //Naviagtion Header content
+        public NavigationDrawerModel AppDetails { get; set; }
+
+        public ObservableCollection<NavigationLinkModel> AppLinks
+        {
+            get { return applinks; }
+            set { applinks = value; }
+        }
+
+        private string Version
+        {
+            get
+            {
+                var assembly = typeof(App).GetTypeInfo().Assembly;
+                var assemblyName = new AssemblyName(assembly.FullName);
+                return assemblyName.Version.Major + "." + assemblyName.Version.Minor + "." + assemblyName.Version.Build + "." + assemblyName.Version.Revision;
+            }
+        }
+
+        #endregion
+
+        #region methods
+
+        public static ContentPage GetSamplesList(string controlName)
+        {
+            return null;
+        }
+
+        internal void GenerateNavigationLinks()
+        {
+            AppDetails = new NavigationDrawerModel();
+            AppDetails.AppVersion = "Version " + Version;
+            AppDetails.AppDesc = string.Empty;
+
+            applinks = new ObservableCollection<NavigationLinkModel>();
+            applinks.Add(new NavigationLinkModel { LinkText = "Product Page", LinkIcons = "productpage.png", LinkURL = "https://www.syncfusion.com/xamarin-ui-controls" });
+            applinks.Add(new NavigationLinkModel { LinkText = "Whats New", LinkIcons = "whatsnew.png", LinkURL = "https://www.syncfusion.com/products/whatsnew/xamarin-forms" });
+            applinks.Add(new NavigationLinkModel { LinkText = "Documentation", LinkIcons = "documentation.png", LinkURL = "https://help.syncfusion.com/xamarin/introduction/overview" });
+        }
+
+        #endregion
+    }
+}
